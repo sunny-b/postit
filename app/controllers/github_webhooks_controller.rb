73c9bd0e777f367caac11e5da724b70a93ac7efd
Sunny Beatteay <sbeatteay@digitalocean.com>
2019-07-15 08:07:05 -0400

    add deploy script

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -1,3 +1,21 @@
 class GithubWebhooksController < ApplicationController
-  def deploy; end
+  skip_before_action :verify_authenticity_token
+
+  GITHUB_USER = 'sunny-b'
+  GITHUB_BRANCH = 'master'
+
+  def deploy
+    if params.fetch(:sender, {}).fetch(:login) == GITHUB_USER && params.fetch(:ref, "").include?(GITHUB_BRANCH)
+      deploy_cmd
+      return render status: :ok, json: @controller.to_json
+    end
+
+    render status: :bad_request, json: {}.to_json
+  end
+
+  private
+
+  def deploy_cmd
+    `#{Rails.root.join('deploy.sh').to_s}`
+  end
 end
